3c499a5edef6117341cf647e07a61a65e45823e6
Reza Rezaee <rezarezaee469@gmail.com>
2024-08-19 19:06:25 +0330

    Staff Panel: Product Management
    Staffs are now able to see, add, edit and delete products

--- OnlineShop/src/accounts/urls.py
+++ OnlineShop/src/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import (CustomerRegisterView, StaffRegsiterView,
-                    StaffPanelView, StaffProfileView,
+from .views import (CustomerRegisterView, StaffRegsiterView, ProductAddView,
+                    StaffPanelView, StaffProfileView, 
+                    ProductListView, ProductDeleteView, ProductEditView,
+                    DeleteImageView,
                     LoginView, LogoutView)
 
 app_name = "accounts"
@@ -14,4 +16,9 @@ urlpatterns = [
     path('dashboard-staff/', StaffPanelView.as_view(), name="dashboard_staff"),
     path('dashboard-staff/profile/', StaffProfileView.as_view(), name="profile_staff"),
     path('dashboard-staff/profile/<int:pk>', StaffProfileView.as_view(), name="profile_staff_edit"),
+    path('dashboard-staff/products/', ProductListView.as_view(), name="profile_staff_product"),
+    path('dashboard-staff/products/create', ProductAddView.as_view(), name="profile_staff_product_create"),
+    path('dashboard-staff/products/edit/<int:pk>/', ProductEditView.as_view(), name="profile_staff_product_edit"),
+    path('dashboard-staff/products/delete/<int:pk>/', ProductDeleteView.as_view(), name="profile_staff_product_delete"),
+    path('dashboard-staff/images/delete/', DeleteImageView.as_view(), name="profile_staff_image_delete"),
 ]
